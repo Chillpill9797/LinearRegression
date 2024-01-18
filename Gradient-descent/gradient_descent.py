b2ff63712fa2877fb7c327ade0a421acc276eb6e
Chillpill9797 <adityagoswami193@gmail.com>
2024-01-18 16:19:36 +0530

    finding cost of each iteration

--- Gradient-descent/gradient_descent.py
+++ Gradient-descent/gradient_descent.py
@@ -3,16 +3,17 @@ import numpy as np
 
 def gradient_descent(x, y):
     m_curr = b_curr = 0
-    iterations = 1000
+    iterations = 10000
     n = len(x)
-    learning_rate = 0.001
+    learning_rate = 0.08
     for i in range(iterations):
         y_predicted = m_curr * x + b_curr
-        md = (1/n)*sum(x*(y_predicted - y))
-        bd = (1/n)*sum(y_predicted - y)
+        cost = (1/n) * sum([val**2 for val in (y - y_predicted)])
+        md = (2/n)*sum(x*(y_predicted - y))
+        bd = (2/n)*sum(y_predicted - y)
         m_curr = m_curr - learning_rate*md
         b_curr = b_curr - learning_rate*bd
-        print(f"m {m_curr}, b {b_curr}, iteration {i}")
+        print(f"m {m_curr}, b {b_curr}, cost {cost}, iteration {i}")
 
 
 x = np.array([1, 2, 3, 4, 5])
