8cae9a6c42992e8ebce228730b7d278ed347f14a
Chillpill9797 <adityagoswami193@gmail.com>
2024-01-18 16:07:33 +0530

    printing values for each iteration

--- Gradient-descent/gradient_descent.py
+++ Gradient-descent/gradient_descent.py
@@ -6,6 +6,13 @@ def gradient_descent(x, y):
     iterations = 1000
     n = len(x)
     learning_rate = 0.001
+    for i in range(iterations):
+        y_predicted = m_curr * x + b_curr
+        md = (1/n)*sum(x*(y_predicted - y))
+        bd = (1/n)*sum(y_predicted - y)
+        m_curr = m_curr - learning_rate*md
+        b_curr = b_curr - learning_rate*bd
+        print(f"m {m_curr}, b {b_curr}, iteration {i}")
 
 
 x = np.array([1, 2, 3, 4, 5])
